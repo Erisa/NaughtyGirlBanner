dcdb9cb47d1358f5cf939e3f4dc2a9452008c66f
Erisa Arrowsmith <seriel@erisa.moe>
2019-12-08 18:40:22 +0000

    Some automatic formatting

--- banner.rb
+++ banner.rb
@@ -1,25 +1,27 @@
-module AntiRaid
-  require "discordrb"
-  require "yaml"
+# frozen_string_literal: true
 
-  @config = YAML.load_file("config.yml")
+module AntiRaid
+  require 'discordrb'
+  require 'yaml'
+
+  @config = YAML.load_file('config.yml')
 
   count = @config[:servers].count
-  puts "[INFO] Loading with #{count} server#{"s" if count != 1} configured!"
+  puts "[INFO] Loading with #{count} server#{'s' if count != 1} configured!"
 
   @bot = Discordrb::Bot.new(token: @config[:token])
 
   @bot.ready do |event|
-    puts "[INFO] Bot is ready!"
-    puts "[INFO] Found the following servers:"
-    @config[:servers].each_key { |sid|
+    puts '[INFO] Bot is ready!'
+    puts '[INFO] Found the following servers:'
+    @config[:servers].each_key do |sid|
       server = event.bot.server(sid)
       if server.nil?
         puts "[WARN] - Server with ID #{sid} not recognised. Bot may not function as expected."
       else
         puts "[INFO] - #{event.bot.server(sid).name} (#{sid}) - #{server.members.count} Members"
       end
-    }
+    end
   end
 
   @bot.message do |event|
@@ -27,21 +29,25 @@ module AntiRaid
 
     begin
       diff = Time.now - event.user.on(event.server).joined_at
-    rescue => exception
-      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{event.server.name} (`#{event.server.id}`): ```ruby\n#{exception}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
-      diff = 9999999999
+    rescue StandardError => e
+      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{event.server.name} (`#{event.server.id}`): ```ruby\n#{e}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
+      diff = 9_999_999_999
     end
 
-    if (@config[:servers][event.server.id][:blacklist] == true && diff < @config[:join_threshold] && @config[:blacklist].any? { |word| event.message.content.downcase.include?(word.downcase) })
-      event.message.delete rescue nil
+    if @config[:servers][event.server.id][:blacklist] == true && diff < @config[:join_threshold] && @config[:blacklist].any? { |word| event.message.content.downcase.include?(word.downcase) }
+      begin
+        event.message.delete
+      rescue StandardError
+        nil
+      end
       puts "[INFO] Crossbanning #{event.user.id}."
-      self.crossban(event.user, event.server)
+      crossban(event.user, event.server)
     elsif @config[:servers][event.server.id][:hard_blacklist] == true && @config[:hard_blacklist].any? { |word| event.message.content.downcase.include?(word.downcase) }
       begin
         event.message.delete
-      rescue => exception
-        puts "[ERROR] Can't delete message by #{event.user.id} in #{event.server.id}: #{exception}"
-        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{event.server.name} (`#{event.server.id}`): ```ruby\n#{exception}```")
+      rescue StandardError => e
+        puts "[ERROR] Can't delete message by #{event.user.id} in #{event.server.id}: #{e}"
+        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{event.server.name} (`#{event.server.id}`): ```ruby\n#{e}```")
         next
       end
       msg = event.respond "#{event.user.mention} pls dont say mean words :(("
@@ -50,12 +56,10 @@ module AntiRaid
     end
   end
 
-  def self.crossban(user, origin_server = nil, invoker = nil, automated = true, reason = "No reason provided.")
-    if invoker.nil?
-      invoker = @bot.user(@config[:owner])
-    end
+  def self.crossban(user, origin_server = nil, invoker = nil, automated = true, reason = 'No reason provided.')
+    invoker = @bot.user(@config[:owner]) if invoker.nil?
 
-    @config[:servers].each_key { |sid|
+    @config[:servers].each_key do |sid|
       server = @bot.server(sid)
       if automated
         full_reason = "[ Automated crossban by #{invoker.distinct} ]: Possible raid message in #{origin_server.name}."
@@ -68,20 +72,19 @@ module AntiRaid
       end
       begin
         server.ban(user, 7, reason: full_reason)
-      rescue => exception
-        puts "[ERROR^] #{exception}"
-        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{server.name} (`#{sid}`): ```ruby\n#{exception}```")
+      rescue StandardError => e
+        puts "[ERROR^] #{e}"
+        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{server.name} (`#{sid}`): ```ruby\n#{e}```")
       end
-    }
+    end
   end
-  
+
   # why did you enable this
   if @config[:debug]
     require 'pry'
     @bot.run(true)
     binding.pry
-  else 
+  else
     @bot.run
   end
-
 end

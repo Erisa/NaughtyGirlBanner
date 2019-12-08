1af0b08723e912693179da4d52783392a760d7d3
Erisa Arrowsmith <seriel@erisa.moe>
2019-12-08 18:46:56 +0000

    Add support for custom server names

--- banner.rb
+++ banner.rb
@@ -19,7 +19,8 @@ module AntiRaid
       if server.nil?
         puts "[WARN] - Server with ID #{sid} not recognised. Bot may not function as expected."
       else
-        puts "[INFO] - #{event.bot.server(sid).name} (#{sid}) - #{server.members.count} Members"
+        print "[INFO] - #{self.server_name(event.bot.server(sid))} (#{sid})"
+        puts " - #{server.members.count} Members"
       end
     end
   end
@@ -30,7 +31,7 @@ module AntiRaid
     begin
       diff = Time.now - event.user.on(event.server).joined_at
     rescue StandardError => e
-      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{event.server.name} (`#{event.server.id}`): ```ruby\n#{e}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
+      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{self.server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
       diff = 9_999_999_999
     end
 
@@ -47,7 +48,7 @@ module AntiRaid
         event.message.delete
       rescue StandardError => e
         puts "[ERROR] Can't delete message by #{event.user.id} in #{event.server.id}: #{e}"
-        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{event.server.name} (`#{event.server.id}`): ```ruby\n#{e}```")
+        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{self.server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```")
         next
       end
       msg = event.respond "#{event.user.mention} pls dont say mean words :(("
@@ -62,21 +63,25 @@ module AntiRaid
     @config[:servers].each_key do |sid|
       server = @bot.server(sid)
       if automated
-        full_reason = "[ Automated crossban by #{invoker.distinct} ]: Possible raid message in #{origin_server.name}."
+        full_reason = "[ Automated crossban by #{invoker.distinct} ]: Possible raid message in #{self.server_name(origin_server)}."
       elsif origin_server.nil?
         full_reason = "[ Crossban by #{invoker.distinct} in Unknown Server ]: #{reason}"
       elsif origin_server == server
         full_reason = "[ Crossban by #{invoker.distinct} ]: #{reason}"
       else
-        full_reason = "[ Crossban by #{invoker.distinct} in #{origin_server.name}]: #{reason}"
+        full_reason = "[ Crossban by #{invoker.distinct} in #{self.server_name(origin_server)}]: #{reason}"
       end
       begin
         server.ban(user, 7, reason: full_reason)
       rescue StandardError => e
         puts "[ERROR^] #{e}"
-        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{server.name} (`#{sid}`): ```ruby\n#{e}```")
+        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{self.server_name(server)} (`#{sid}`): ```ruby\n#{e}```")
       end
     end
+  end
+
+  def self.server_name(server)
+    @config[:servers][server.id][:name].nil? ? server.name : @config[:servers][server.id][:name]
   end
 
   # why did you enable this

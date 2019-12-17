a6ff182e9420dc1ff93ba6a4e5910ebb6853ccd9
Erisa Arrowsmith <seriel@erisa.moe>
2019-12-17 13:52:00 +0000

    Auto formatting

--- banner.rb
+++ banner.rb
@@ -17,7 +17,7 @@ module AntiRaid
       if server.nil?
         puts "[WARN] - Server with ID #{sid} not recognised. Bot may not function as expected."
       else
-        print "[INFO] - #{self.server_name(event.bot.server(sid))} (#{sid})"
+        print "[INFO] - #{server_name(event.bot.server(sid))} (#{sid})"
         puts " - #{server.members.count} Members"
       end
     end
@@ -27,18 +27,19 @@ module AntiRaid
   @bot.message do |event|
     next if @config[:servers][event.server.id].nil?
 
-    if event.message.content.start_with?("ngb ping")
+    if event.message.content.start_with?('ngb ping')
       return_message = event.respond('Pinging..!')
-        ping = (return_message.id - event.message.id) >> 22
-	      choose = %w(i o e u y a)
-        return_message.edit("P#{choose.sample}ng! (`#{ping}ms`)")
+      ping = (return_message.id - event.message.id) >> 22
+      choose = %w[i o e u y a]
+      return_message.edit("P#{choose.sample}ng! (`#{ping}ms`)")
     end
 
-    if event.message.content.start_with?("ngb eval")
+    if event.message.content.start_with?('ngb eval')
       next if event.user.id != @config[:owner]
+
       args = event.message.content.delete_prefix('ngb eval ').split(' ')
       begin
-        msg = event.respond "Evaluating..."
+        msg = event.respond 'Evaluating...'
         init_time = Time.now
         result = eval args.join(' ')
         result = result.to_s
@@ -49,21 +50,20 @@ module AntiRaid
         str = ''
         if result.length >= 1984
           str << "Your output exceeded the character limit! (`#{result.length - 1984}`/`1984`)"
-          str << "But hastebin support was not implemented. Sorry."
+          str << 'But hastebin support was not implemented. Sorry.'
         else
           str << "Output: ```\n#{result}```Command took #{(Time.now - init_time)} seconds to execute!"
         end
         msg.edit(str)
-        rescue StandardError => e
+      rescue StandardError => e
         msg.edit("An error has occured!! ```ruby\n#{e}```\nCommand took #{(Time.now - init_time)} seconds to execute!")
       end
     end
 
-
     begin
       diff = Time.now - event.user.on(event.server).joined_at
     rescue StandardError => e
-      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{self.server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
+      @bot.user(@config[:owner]).pm("Error occured trying to process join time for <@#{event.user.id}> in #{server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```\nJoined at: `#{event.user.on(event.server).joined_at}`")
       diff = 9_999_999_999
     end
 
@@ -80,7 +80,7 @@ module AntiRaid
         event.message.delete
       rescue StandardError => e
         puts "[ERROR] Can't delete message by #{event.user.id} in #{event.server.id}: #{e}"
-        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{self.server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```")
+        @bot.user(@config[:owner]).pm("Error occured trying to delete message by <@#{event.user.id}> from #{server_name(event.server)} (`#{event.server.id}`): ```ruby\n#{e}```")
         next
       end
       msg = event.respond "#{event.user.mention} pls dont say mean words :(("
@@ -95,19 +95,19 @@ module AntiRaid
     @config[:servers].each_key do |sid|
       server = @bot.server(sid)
       if automated
-        full_reason = "[ Automated crossban by #{invoker.distinct} ]: Possible raid message in #{self.server_name(origin_server)}."
+        full_reason = "[ Automated crossban by #{invoker.distinct} ]: Possible raid message in #{server_name(origin_server)}."
       elsif origin_server.nil?
         full_reason = "[ Crossban by #{invoker.distinct} in Unknown Server ]: #{reason}"
       elsif origin_server == server
         full_reason = "[ Crossban by #{invoker.distinct} ]: #{reason}"
       else
-        full_reason = "[ Crossban by #{invoker.distinct} in #{self.server_name(origin_server)}]: #{reason}"
+        full_reason = "[ Crossban by #{invoker.distinct} in #{server_name(origin_server)}]: #{reason}"
       end
       begin
         server.ban(user, 7, reason: full_reason)
       rescue StandardError => e
         puts "[ERROR^] #{e}"
-        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{self.server_name(server)} (`#{sid}`): ```ruby\n#{e}```")
+        @bot.user(@config[:owner]).pm("Error occured trying to ban <@#{user.id}> from #{server_name(server)} (`#{sid}`): ```ruby\n#{e}```")
       end
     end
   end
